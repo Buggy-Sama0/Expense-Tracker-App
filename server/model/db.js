8cb9d9ef47efe57af54b171fceee7f3dfb7074f0
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-05-27 14:37:46 +0800

    / commit

--- server/model/db.js
+++ server/model/db.js
@@ -2,16 +2,13 @@ require('dotenv').config();
 // For MySQL
 let mysql=require('mysql');
 
-// Create connection pool instead of single connection
-const connection = mysql.createPool({
+const connection=mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0
+    database: process.env.DB_NAME
 });
+
 connection.connect((err)=>{
     if (err) {
         console.error('Error connecting to database:', err);
