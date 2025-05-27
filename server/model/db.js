8ea4b301f58dba30ff884be60c76c5bc68365bb9
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-05-27 14:30:14 +0800

    / commit

--- server/model/db.js
+++ server/model/db.js
@@ -3,14 +3,17 @@ require('dotenv').config();
 let mysql=require('mysql');
 
 const connection=mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'expense'
+    database: process.env.DB_NAME
 });
 
 connection.connect((err)=>{
-    if (err) return console.log(err.message);
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
 
     console.log('Connected to the MySql server'); 
 });
