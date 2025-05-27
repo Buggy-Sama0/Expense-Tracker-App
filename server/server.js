62612c217e1493a585ba8cdc424c23cc7da55419
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-05-27 12:41:26 +0800

    / commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const mysqlConnection=require('./model/db');
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+        'https://expense-app-tracker1.netlify.app/'
+    ]
 };
 
 app.use(cors(corsOptions));
